7285698ff3f1b6bb0b2ce8cee2a1f41aa0546a29
Blake Redwolf <blakethedead@gmail.com>
2018-03-14 03:25:55 -0800

    First Anioval Fit Finder commit. (Beta)

---
+++ js/slider.js
@@ -0,0 +1,8 @@
+// With JQuery
+$("#ex13").slider({
+    ticks: [0, 1, 2, 3, 4],
+    ticks_labels: ['Very Tight', 'Tight', 'Regular', 'Loose', 'Very Loose'],
+    ticks_snap_bounds: 30
+});
+
+$(".bb-div").css("style: width=100%");
